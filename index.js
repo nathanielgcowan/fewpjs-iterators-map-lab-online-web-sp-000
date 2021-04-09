279c5f9ab6b93b21ab21ca017ec142e5e2fee871
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-04-09 13:39:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,10 +11,12 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+
+
 function titleCased() {
   return tutorials.map(function (tutorial) {
     tutorial = tutorial.split(' ');
-    for (var i = 0; i < tutorial.length; i++) {
+    for (let i = 0; i < tutorial.length; i++) {
       tutorial[i] = tutorial[i].charAt(0).toUpperCase() + tutorial[i].slice(1);
     }
     return tutorial.join(' ');
